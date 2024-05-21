10562e910d44404ace5dce6a507b8afe695400be
Henrique Panham <henriquepjv@hotmail.com>
2024-05-21 11:40:11 -0300

    Add campaign required params validation

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"time"
 
 	"github.com/rs/xid"
@@ -18,7 +19,16 @@ type Campaign struct {
 	Contacts  []Contact
 }
 
-func NewCampaign(name string, content string, emails []string) *Campaign {
+// func nome_da_func(params) o_que_vou_retornar {....}
+func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
+	if name == "" {
+		return nil, errors.New("name is required")
+	} else if content == "" {
+		return nil, errors.New("content is required")
+	} else if len(emails) == 0 {
+		return nil, errors.New("at least one email is required")
+	}
+
 	contacts := make([]Contact, len(emails)) // creates the array with defined range
 	for index, email := range emails {       // populate
 		contacts[index].Email = email
@@ -30,5 +40,5 @@ func NewCampaign(name string, content string, emails []string) *Campaign {
 		Content:   content,
 		CreatedAt: time.Now(),
 		Contacts:  contacts,
-	}
+	}, nil // return do valor do erro
 }
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -10,19 +10,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ( // const only accept primitive kinds, var creates a slice that should not be modified
+	Name     = "Campaign 1"
+	Content  = "Body"
+	Contacts = []string{"email1@bol.com", "email2@bol.com"} // creates a slice of strings
+)
+
 func Test_NewCampaign_Creation(t *testing.T) {
 	assert := assert.New(t) // external lib testify
-	name := "Campaign 1"
-	content := "Body"
-	contacts := []string{"email1@bol.com", "email2@bol.com"} // creates a slice of strings
 	now := time.Now().Add(-time.Minute)
 
-	campaign := NewCampaign(name, content, contacts)
+	campaign, _ := NewCampaign(Name, Content, Contacts)
 
 	assert.NotNil(campaign.ID)
 	assert.NotNil(campaign.CreatedAt)
 	assert.Greater(campaign.CreatedAt, now)
-	assert.Equal(name, campaign.Name)
-	assert.Equal(content, campaign.Content)
-	assert.Equal(len(contacts), len(campaign.Contacts))
+	assert.Equal(Name, campaign.Name)
+	assert.Equal(Content, campaign.Content)
+	assert.Equal(len(Contacts), len(campaign.Contacts))
+}
+
+func Test_NewCampaign_Validate_Name(t *testing.T) {
+	assert := assert.New(t) // external lib testify
+
+	_, err := NewCampaign("", Content, Contacts)
+
+	assert.Equal("name is required", err.Error())
+}
+
+func Test_NewCampaign_Validate_Content(t *testing.T) {
+	assert := assert.New(t) // external lib testify
+
+	_, err := NewCampaign(Name, "", Contacts)
+
+	assert.Equal("content is required", err.Error())
+}
+
+func Test_NewCampaign_Validate_Contacts(t *testing.T) {
+	assert := assert.New(t) // external lib testify
+
+	_, err := NewCampaign(Name, Content, []string{})
+
+	assert.Equal("at least one email is required", err.Error())
 }
