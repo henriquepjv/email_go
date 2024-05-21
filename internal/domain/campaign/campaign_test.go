301d20f8473d10e9626213f66444b4bfba6a10fb
Henrique Panham <henriquepjv@hotmail.com>
2024-05-21 10:51:00 -0300

    Add testify lib

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -3,18 +3,22 @@
 
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t) // external lib testify
 	name := "Campaign 1"
 	content := "Body"
 	contacts := []string{"email1@bol.com", "email2@bol.com"} // creates a slice of strings
 
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID != 1 {
-		t.Errorf("expected 1")
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("expected correct contacts")
-	}
+	assert.Equal(1, campaign.ID)
+	assert.Equal(name, campaign.Name)
+	assert.Equal(content, campaign.Content)
+	assert.Equal(len(contacts), len(campaign.Contacts))
 }
